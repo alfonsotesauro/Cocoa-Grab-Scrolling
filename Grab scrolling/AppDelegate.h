ae1e518af3fa619b8908c6fc428690f57d63096e
alfonsotesauro <alfonso.tesauro@alfonsotesauro.net>
2015-05-02 17:27:38 +0200

    Initial Commit

---
+++ Grab scrolling/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Grab scrolling
+//
+//  Created by Alfonso Maria Tesauro on 02/05/15.
+//  Copyright (c) 2015 Alfonso Tesauro. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Grab scrolling/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Grab scrolling
+//
+//  Created by Alfonso Maria Tesauro on 02/05/15.
+//  Copyright (c) 2015 Alfonso Tesauro. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
